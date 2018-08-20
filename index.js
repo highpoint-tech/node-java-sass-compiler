bfd1e8f98a8e5ebc3aab0657b91d5f02835fc548
Jonathan Rehm <jrehm@mhighpoint.com>
2018-08-20 16:52:17 -0700

    Change module export

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 const exec = require('child_process').exec;
 const os = require('os');
 
-function compile(opts, cb) {
-  /* eslint-disable no-param-reassign */
-  opts = Object.assign({
-    compress: false,
-    ignoreWarnings: false,
-    in: '',
-    minify: false,
-    out: '',
-    urlMode: false
-  }, opts || {});
+const compileCallback = (
+  {
+    compress = false,
+    ignoreWarnings = false,
+    in: pathIn = '',
+    minify = false,
+    out: pathOut = '',
+    urlMode = false
+  },
+  cb
+) => {
+  let cmd = os.platform() === 'win32' ? 'java' : '/usr/bin/env java';
+  cmd = `${cmd} -cp ${__dirname}/vaadin-sass-compiler.jar \
+com.vaadin.sass.SassCompiler ${pathIn} ${pathOut}`;
 
-  let cmd = (os.platform() === 'win32') ? 'java' : '/usr/bin/env java';
-  cmd = `${cmd} -cp ${__dirname}/vaadin-sass-compiler.jar com.vaadin.sass.SassCompiler \
-${opts.in} ${opts.out}`;
-
-  if (opts.minify) {
+  if (minify) {
     cmd = `${cmd} -minify:true`;
   }
 
-  if (opts.compress && opts.out.length > 0) {
+  if (compress && pathOut.length > 0) {
     cmd = `${cmd} -compress:true`;
   }
 
-  if (opts.urlMode) {
-    cmd = `${cmd} -urlMode:${opts.urlMode}`;
+  if (urlMode) {
+    cmd = `${cmd} -urlMode:${urlMode}`;
   }
 
-  if (opts.ignoreWarnings) {
+  if (ignoreWarnings) {
     cmd = `${cmd} -ignore-warnings:true`;
   }
 
   exec(cmd, cb);
-}
+};
 
-module.exports = compile;
+const compile = opts =>
+  new Promise((resolve, reject) =>
+    compileCallback(opts, err => {
+      if (err) return reject(err);
+      resolve();
+    })
+  );
+
+module.exports = {
+  compile,
+  compileCallback,
+  default: compile
+};
